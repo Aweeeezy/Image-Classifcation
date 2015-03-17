94de8b454c51a569aa57278ab0ae59c59c23c75c
Alex Richards <aweeeezy@Alexs-MBP.att.net>
2015-03-16 17:57:41 -0700

    Beginning template

---
+++ image_processor.py
@@ -0,0 +1,24 @@
+"""
+    Module for storing processed data in an organized fashion. Structured data
+    allows easy application of controlled data sets to machine learning models
+    for both training and testing.
+"""
+
+class image_processor():
+    """ This class is the summation of all the built structures. Each structure
+    is constructed by call `Images().<structure_name>` via the `@property`
+    decorator. """
+
+    healthy_unhealthy = {} # simple binary classification <INDICATOR> = '@'
+    unhealthy_degree = {} # ranked 1-4                   <INDICATOR> = '$'
+    all_samples = {} =     # unstructured                 <INDICATOR> = '%'
+    groups = {'@':healthy_unhealthy, '$':unhealthy_degree, '%':all_samples}
+
+    def __init__(self, in_data):
+        """ Creates data objects for each sample and maps them as values to a
+        key, their ID, for data_structure (dictionary) """
+        with open('structured_data', 'r') as f:
+            [create_structure(line, f) for line in f if line is in groups]
+
+    def create_structure(self, indicator, f):
+        """ arg `indicator` determines which structure dictionary is populated """
